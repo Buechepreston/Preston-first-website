f672ee255f6b538d3bbe0e5342e68c0d3ae1f46f
Buechepreston <127057139+Buechepreston@users.noreply.github.com>
2025-04-26 22:08:50 -0400

    Add files via upload

--- interaction.js
+++ interaction.js
@@ -1,5 +1,3 @@
 <button 
-    onclick="alert('Congratulations! 
-    You clicked the button!
-    I  dont know how to read your comment yet but thanks for particapting')">
+    onclick="alert('You clicked the button! I dont know how to read your comment yet but thanks for particapting')">
     Submit</button> 
