803fdf58164f720bcd9a906e06654b498106103e
Buechepreston <127057139+Buechepreston@users.noreply.github.com>
2025-06-05 18:13:11 -0400

    Delete interaction.js

--- interaction.js
+++
@@ -1,9 +0,0 @@
-// (Optional: Move the JS from index.html here for organization.)
-// Show/hide back-to-top button
-window.addEventListener('scroll', function() {
-  document.getElementById('backToTop').style.display =
-    window.scrollY > 300 ? 'block' : 'none';
-});
-document.getElementById('backToTop').onclick = function() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
-};
